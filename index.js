d0d1ec02c92fedba0b6b59e6c2a8072995e3135c
Eren Dogan <eren.dogan@gaziulas.com.tr>
2019-06-05 11:08:44 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,9 +2,9 @@ var1 = 31;
 var2 = 2;
 var multiply = var1 * var2;
 
-var random = Math.random()+1;
+let random = Math.random()+1;
 
-var mod = var1 % 9;
+let mod = var1 % 9;
 
-var max = Math.max(4,8,10,20);
+let max = Math.max(4,8,10,20);
 
